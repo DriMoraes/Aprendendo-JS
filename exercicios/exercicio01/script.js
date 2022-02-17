ec6b2fbfe2caba9c370a99d236c88b69b7e284f5
DriMoraes <adrieledemoraes@gmail.com>
2022-02-17 12:18:50 -0300

    update

--- exercicios/exercicio01/script.js
+++ exercicios/exercicio01/script.js
@@ -3,24 +3,26 @@ function carregar()
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()    
+    //var hora = data.getHours()  
+    var hora = 21
     msg.innerHTML = `Agora são ${hora} horas.`
+
     if (hora >= 0 && hora < 12)
     {
         //Bom Dia!
-        img.src = 'img/manha.jpg'
-        document.body.style.background = '#61a2c2'
+        img.src = 'img/manha.png'
+        document.body.style.background = '#efddc5'
     }
     else if (hora >= 12 && hora <= 18)
     {
         //Boa Tarde!
-        img.src = 'img/tarde.jpg'
-        document.body.style.background = '#b29786'
+        img.src = 'img/tarde.png'
+        document.body.style.background = '#f3d8d1'
     }
     else 
     {
         //Boa noite!
-        img.src = 'img/noite.jpg'
-        document.body.style.background = '#1a2d4d'
+        img.src = 'img/noite.png'
+        document.body.style.background = '#4e609e'
     }
 }
